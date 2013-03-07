7813872bd06aa847db785968a693ed0b34084c86
weedazar <1pindsvin@gmail.com>
2013-03-07 21:07:10 +0100

    Ensure checkin before sync

---
+++ csharp/VifManager/src/XmlConverter/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace dk.magnus.xml
+{
+
+    class XmlConverter
+    {
+        public XmlConverter()
+        {
+            
+        }   
+  
+        void ReadXml(IEnumerable<string> lines)
+        {
+            
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+        }
+    }
+}
